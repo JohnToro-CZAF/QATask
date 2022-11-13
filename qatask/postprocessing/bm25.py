fff7259d9bd3036ad49bab6e3bdb8c798503fcd3
JohnToro-CZAF <johnphan1608@gmail.com>
2022-11-13 18:01:28 +0000

    String matching snippet and Date transform

--- qatask/postprocessing/bm25.py
+++ qatask/postprocessing/bm25.py
@@ -5,6 +5,7 @@ import os
 import os.path as osp
 import sqlite3
 from tqdm import tqdm
+from fuzzywuzzy import process
 
 class BM25PostProcessor(BasePostProcessor):
     def __init__(self, cfg, db_path):
@@ -16,46 +17,11 @@ class BM25PostProcessor(BasePostProcessor):
         con = sqlite3.connect(osp.join(os.getcwd(), db_path))
         self.cur = con.cursor()
     
-    def process_year(self, ans):
-        if "năm" or "Năm" in ans:
-            return ans
-        else:
-            return None
-
-    def process(self, data):
-        print("Postprocessing...")
-        for question in tqdm(data["data"]):
-            # if self.process_year(question['answer']) is not None:
-            #     question['answer'] = self.process_year(question['answer'])
-            #     continue
-            hits = self.searcher.search(question['answer'])
-            try:
-                doc_id = hits[0].docid
-                res = self.cur.execute("SELECT wikipage FROM documents WHERE id = ?", (str(doc_id), ))
-                wikipage = res.fetchone()
-                print(wikipage)
-                question['answer'] = wikipage
-            except:
-                print("can no retrieve this question wikipage")
-             
-        return data
-
-    def __call__(self, data):
-        return self.process(data)
-
-class HoangPostProcessor(object):
-    """Base class for post processors."""
- 
-    def __init__(self, cfg=None):
-        """Initialize the post processor.
- 
-        Args:
-            config (dict): Configuration for the post processor.
-        """
-        self.cfg = cfg
- 
     def checktype(self, text):
+        text = text.lower()
         """Check if the text is a date or a number."""
+        if text == "":
+            return 3
         words = text.split()
         for w in words:
             if w == 'năm' or w == 'tháng' or w == 'ngày':
@@ -63,7 +29,9 @@ class HoangPostProcessor(object):
         if len(words) == 1 and words[0].isdigit():
             return 1
         return 0
+
     def date_transform(self, text):
+        text = text.lower()
         words = text.split()
         lookup = {'năm': '', 'tháng': '', 'ngày': ''}
         for idx, w in enumerate(words):
@@ -77,39 +45,47 @@ class HoangPostProcessor(object):
             ans += 'tháng ' + lookup['tháng'] + " "
         if lookup['năm'] != "":
             ans += 'năm ' + lookup['năm'] + " "
-        áns = ans.strip()
+        ans = ans.strip()
         return ans
- 
-    def postprocess(self, text):
-        if text == '':
-            return 'null'
-        anstype = self.checktype(text)
-        if anstype == 2:
-            return self.date_transform(text)
-        elif anstype == 1:
-            # Only number
-            return text
-        elif anstype == 0:
-            return 'wiki/'+text.replace(" ", "_")
-        else:
-            assert("Error")
- 
+
     def process(self, data):
-        """Process the data.
- 
-        Args:
-            data (dict): List of {question, answers:List, scores:List}
- 
-        Returns:
-            dict: Processed data.
-        """
-        return self.postprocess(data)
- 
+        print("Postprocessing...")
+        for question in tqdm(data["data"]):
+            anstype = self.checktype(question['answer'])
+            if anstype > 0:
+                if anstype == 3:
+                    question['answer'] = 'null'
+                elif anstype == 2:
+                    question['answer'] = self.date_transform(question['answer'])
+                elif anstype == 1:
+                    pass
+                continue
+            hits = self.searcher.search(question['answer'])
+            try:
+                doc_id = hits[0].docid
+                res = self.cur.execute("SELECT wikipage FROM documents WHERE id = ?", (str(doc_id), ))
+                wikipage = res.fetchone()
+                print(wikipage)
+                # TODO: There are many canidate wikipage -> find the one who is 
+                # nearest
+                # doc_ids = [(str(hit.docid), ) for hit in hits]
+                # res = self.cur.execute("SELECT wikipage FROM documents WHERE id = ?", (doc_ids, ))
+                # wikipages = res.fetchall()
+                # choices = [wikipage[0][5:].replace("_", " ") for wikipage in wikipages]
+                # wikipage = process.extractOne(question['answer], choices)[0]
+                # question['answer'] = 'wiki/' + wikipage.replace(" ", "_")
+                question['answer'] = wikipage
+            except:
+                print("can no retrieve this question wikipage")
+             
+        return data
+
     def __call__(self, data):
         return self.process(data)
+
 def main():
     processor = BasePostProcessor()
     text = "ngày 1 tháng 2 năm 2019"
-    print(processor(text))
+    print(processor.date_transform(text))
 if __name__ == '__main__':
     main()
