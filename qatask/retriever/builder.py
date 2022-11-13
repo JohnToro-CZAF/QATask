025bbd089511c1126f367ca9015000c94f262ffe
huyphan168 <zalarus.pbc168@gmail.com>
2022-11-13 12:38:48 +0700

    update

--- qatask/retriever/builder.py
+++ qatask/retriever/builder.py
@@ -1,15 +1,15 @@
 from .tfidf_retriever import TFIDFRetriever
-from .serini_retriever import ColbertRetriever, DPRRetriever, ANCERetriever
+# from .serini_retriever import ColbertRetriever, DPRRetriever, ANCERetriever
 
 def build_retriever(cfg, tokenizer, db_path):
     if cfg.type == "tf-idf":
         return TFIDFRetriever(cfg, tokenizer, db_path)
-    elif cfg.type == "colbertv2":
-        return ColbertRetriever(cfg.index_path, cfg.top_k, db_path)
-    elif cfg.type == "dpr":
-        return DPRRetriever(cfg.index_path, cfg.top_k, db_path)
-    elif cfg.type == "ance":
-        return ANCERetriever(cfg.index_path, cfg.top_k, db_path)
-    else:
-        assert cfg.type == "default", "NotImplemented retriever{}".format(cfg.type)
+    # elif cfg.type == "colbertv2":
+    #     return ColbertRetriever(cfg.index_path, cfg.top_k, db_path)
+    # elif cfg.type == "dpr":
+    #     return DPRRetriever(cfg.index_path, cfg.top_k, db_path)
+    # elif cfg.type == "ance":
+    #     return ANCERetriever(cfg.index_path, cfg.top_k, db_path)
+    # else:
+    #     assert cfg.type == "default", "NotImplemented retriever{}".format(cfg.type)
     
